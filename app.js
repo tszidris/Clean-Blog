9ad913162e3ddbe6fe7d2799028bee07a357fed6
tszidris <idrisyyildirim@gmail.com>
2023-07-06 19:10:01 +0300

    daily update

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Merhaba");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log("Sunucu Çalıştırıldı");
+});
