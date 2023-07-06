8f08e7b69f5cc39731def5150bb89aaa35edb120
tszidris <idrisyyildirim@gmail.com>
2023-07-07 02:05:19 +0300

    commit3

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
 
 const app = express();
 
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Merhaba");
+  res.render("index");
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
 
 const port = 3000;
